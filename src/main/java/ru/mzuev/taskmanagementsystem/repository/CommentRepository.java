4aacac0b4d63d1552693ec411fc0a6b220348316
mzuev <mszuev@mail.ru>
2025-02-26 01:58:07 +0300

    add repositories

---
+++ src/main/java/ru/mzuev/taskmanagementsystem/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package ru.mzuev.taskmanagementsystem.repository;
+
+import ru.mzuev.taskmanagementsystem.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByTaskId(Long taskId, Pageable pageable);
+}
---
+++ src/main/java/ru/mzuev/taskmanagementsystem/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package ru.mzuev.taskmanagementsystem.repository;
+
+import ru.mzuev.taskmanagementsystem.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    Page<Task> findByAuthorId(Long authorId, Pageable pageable);
+    Page<Task> findByExecutorId(Long executorId, Pageable pageable);
+}
