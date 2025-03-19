b277fb3320be0ccd4746816dbad923265fc2f239
mzuev <mszuev@mail.ru>
2025-03-19 03:05:24 +0300

    add DTO class for - Comment - Task - User

---
+++ src/main/java/ru/mzuev/taskmanagementsystem/dto/CommentDTO.java
@@ -0,0 +1,40 @@
+package ru.mzuev.taskmanagementsystem.dto;
+
+public class CommentDTO {
+    private Long id;
+    private String content;
+    private Long taskId;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/ru/mzuev/taskmanagementsystem/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package ru.mzuev.taskmanagementsystem.dto;
+
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
