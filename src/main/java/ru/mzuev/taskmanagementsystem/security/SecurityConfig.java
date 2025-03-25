7b702dd26aaa14cd94263d6ff6b3e2960ce7cf90
mzuev <mszuev@mail.ru>
2025-03-26 02:46:32 +0300

    edit SecurityConfig - add MessageSource bean to get messages from messages.properties

--- src/main/java/ru/mzuev/taskmanagementsystem/security/SecurityConfig.java
+++ src/main/java/ru/mzuev/taskmanagementsystem/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package ru.mzuev.taskmanagementsystem.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -98,5 +100,13 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
