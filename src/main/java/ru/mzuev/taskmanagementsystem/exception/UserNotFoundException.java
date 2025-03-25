6de2b4413314643c48de4d51cec26f0dfa98de98
mzuev <mszuev@mail.ru>
2025-03-26 02:50:07 +0300

    edit UserNotFoundException - add another constructor

--- src/main/java/ru/mzuev/taskmanagementsystem/exception/UserNotFoundException.java
+++ src/main/java/ru/mzuev/taskmanagementsystem/exception/UserNotFoundException.java
@@ -4,4 +4,7 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String email) {
         super("Пользователь не найден с email " + email);
     }
+    public UserNotFoundException(Long userId) {
+        super("Пользователь не найден с id " + userId);
+    }
 }
