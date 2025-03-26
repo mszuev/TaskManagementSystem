42134146e746d3e75625d311eadf2a460b991ccd
mzuev <mszuev@mail.ru>
2025-03-26 04:50:20 +0300

    refactor(StatusUpdateRequest): add validation constraints

--- src/main/java/ru/mzuev/taskmanagementsystem/dto/StatusUpdateRequest.java
+++ src/main/java/ru/mzuev/taskmanagementsystem/dto/StatusUpdateRequest.java
@@ -1,5 +1,7 @@
 package ru.mzuev.taskmanagementsystem.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StatusUpdateRequest {
+    @NotBlank(message = "{validation.task.status.required}")
+    @Pattern(regexp = "в очереди|в работе|на проверке|завершена",
+            message = "{validation.task.status.pattern}")
     private String status;
 }
